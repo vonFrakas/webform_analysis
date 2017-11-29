97b8a4843effc289007d68fcad9a0852945e51a4
Laurent <lbaran@3368098.no-reply.drupal.org>
2017-11-29 07:35:38 +0100

    Issue #2925863: Review and follow Coding Standards

---
+++ js/webform_analysis.charts.js
@@ -0,0 +1,30 @@
+(function ($, Drupal, settings) {
+  'use strict';
+
+  /**
+   * Webform Analaysis - Charts.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.WebformAnalysisCharts = {
+    attach: function (context, settings) {
+
+      var webformcharts = settings.webformcharts;
+
+      google.charts.load('current', {packages: webformcharts.packages});
+
+      google.charts.setOnLoadCallback(function () {
+
+        webformcharts.charts.forEach(function (chart) {
+
+          var data = new google.visualization.arrayToDataTable(chart.data);
+          var options = chart.options;
+          var gchart = new google.visualization[chart.type](document.querySelector(chart.selector));
+          gchart.draw(data, options);
+
+        });
+      });
+
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
